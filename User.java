38ce9ca635a71e185c63ad7edbd2610ab6fc9888
elfigo <elfigodevelopers@gmail.com>
2016-08-15 17:25:52 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,21 @@
+package com.example.krishnaupadhyay.elfigo;
+
+import java.io.Serializable;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Android on 22-04-2015.
+ */
+public class User {
+
+
+    public String name;
+
+    public String email;
+
+    public String facebookID;
+
+    public String gender;
+
+
+}
